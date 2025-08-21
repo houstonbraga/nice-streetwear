dd84254fd81bfcd56a264b2195963f19b2450ff2
Houston Braga <houstonliverboy123@gmail.com>
2025-08-20 21:11:10 -0300

    feat: add login with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,7 +8,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
     schema, // or "pg" or "mysql"
@@ -21,6 +26,9 @@ export const auth = betterAuth({
   },
   session: {
     modelName: "sessionTable",
-  }, //usar modelNames caso o nome da tabela seja alterada ao colocar tabela de authenticação
+  },
+  verification: {
+    modelName: "verificationTable"
+  } //usar modelNames caso o nome da tabela seja alterada ao colocar tabela de authenticação
   //dentro da tabela principal de dados
 });
